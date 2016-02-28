6fa7e33674326ecde5a7055089a318a50c6615ad
Youthink <hufy3651@163.com>
2016-02-28 22:26:22 +0800

    search form

---
+++ VicSugar-wordpress/searchform.php
@@ -0,0 +1,7 @@
+<form class="search" action="<?php echo home_url( "/" ); ?>" method="get">
+
+    <input type="search"  class="input-search" placeholder="搜索" name="s" id="search" value="<?php the_search_query(); ?>" >
+
+    <i class="icon-search"></i>
+
+</form>
