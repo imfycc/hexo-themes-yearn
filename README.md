50d5a5b26fa3a8a03298dc302b57469ba839dfe7
Youthink <hufy3651@163.com>
2016-04-08 23:41:16 +0800

    @media screen

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
-> VicSugar 是我根据自己的浏览习惯打造的一款主题，设计灵感来自于[简书](http://www.jianshu.com/)、[云龙博客](http://yunlzheng.github.io/)等众多网站。我希望VicSugar具备简洁、炫酷、有阅读感的特点。VicSugar作为我的网站主题，我会经常优化它，把学习到的一些新技术运用到这个主题上。如果网站上有你感兴趣的效果可以参考此主题的wiki.
+> VicSugar 是我根据自己的浏览习惯打造的一款主题，设计灵感来自于[简书](http://www.jianshu.com/)、[云龙博客](http://yunlzheng.github.io/)等众多网站。我希望VicSugar具备简洁、炫酷、有阅读感的特点。VicSugar作为我的网站主题，我会经常优化，把学习到的一些新技术运用到这个主题上。
 
 ### 预览地址：[胡方运的博客](http://hufangyun.com)
 
 ### 下载地址：[Github](https://github.com/Youthink/VicSugar)
 
-### WIKI地址：[Github wiki](https://github.com/Youthink/VicSugar/wiki)
+### 使用说明：
 
-wordpress主题的制作以及主题各种前端效果的实现请参考WIKi
+1、将`VicSugar-wordpress`文件夹压缩为`.zip`格式。
+
+2、在wordpress后台添加主题即可。
 
 
 
--- VicSugar-wordpress/index.php
+++ VicSugar-wordpress/index.php
@@ -5,10 +5,12 @@
     <meta name="baidu_union_verify" content="4a0e189d6aa33c77bf2c7a8fce617ff2"><!--联盟-->
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
 	<meta name="keywords" content="胡方运,程小猿,前端,个人博客,Youthink,Web" />
+	<meta name="description" content="程小猿的个人博客，Web前端工程师的成长之路" />
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?> ">
 	<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/public/img/favicon.ico" title="Favicon">	
 	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/public/css/font-awesome.min.css">
 	<title><?php bloginfo('name'); ?></title>
+
 	<!-- 百度检测-->
 	<script>
 		var _hmt = _hmt || [];
@@ -21,38 +23,38 @@
 	</script>
 </head>
 <body>
-	<div class="left-aside">
+	<header class="left-aside">
 		<div class="face-img"><img src="<?php bloginfo('template_directory'); ?>/public/img/face.jpg"></div>
 		<div class="intro-word">
 			<h2><?php bloginfo('name'); ?></h2>
-			<p>天马行空，爱生活，爱coding</p>
-			<p>The value of life by their own decisions</p>
+			<p class="ms">天马行空，爱生活，爱coding</p>
+			<p class="ms">The value of life by their own decisions</p>
 		</div>
 	    <div class="sns">
             <ul>
                 <li>
                     <a href="http://music.163.com/#/user/home?id=30828384" target="_blank">
-                       <img src="<?php bloginfo('template_directory'); ?>/public/img/iconfont-netease-music.svg" alt="网易云音乐" data-tooltip="网易云音乐">
+                       <img src="<?php bloginfo('template_directory'); ?>/public/img/icon-netease.png" alt="网易云音乐" data-tooltip="网易云音乐">
                     </a>
                 </li>
                 <li>
                     <a>
-                        <img src="<?php bloginfo('template_directory'); ?>/public/img/iconfont-qq.svg" alt="898843651" data-tooltip="QQ:898843651">
+                        <img src="<?php bloginfo('template_directory'); ?>/public/img/icon-qq.png" alt="898843651" data-tooltip="QQ:898843651">
                     </a>
                 </li>
                 <li>
                     <a href="http://youthol.cn" target="_blank">
-                        <img src="<?php bloginfo('template_directory'); ?>/public/img/Youthol.svg" alt="青春在线" data-tooltip="青春在线" >
+                        <img src="<?php bloginfo('template_directory'); ?>/public/img/icon-youthol.png" alt="青春在线" data-tooltip="青春在线" >
                     </a>
                 </li>
                 <li>
                     <a>
-                        <img src="<?php bloginfo('template_directory'); ?>/public/img/iconfont-appicon06.svg" alt="邮箱" data-tooltip="hi@hufangyun.com">
+                        <img src="<?php bloginfo('template_directory'); ?>/public/img/icon-email.png" alt="邮箱" data-tooltip="hi@hufangyun.com">
                     </a>
                 </li>
                 <li>
                     <a href="https://github.com/Youthink" target="_blank">
-                        <img src="<?php bloginfo('template_directory'); ?>/public/img/iconfont-github.svg" alt="GitHub" data-tooltip="GitHub">
+                        <img src="<?php bloginfo('template_directory'); ?>/public/img/icon-github.png" alt="GitHub" data-tooltip="GitHub">
                     </a>
                 </li>
             </ul>
@@ -60,31 +62,27 @@
                 <p></p><span></span>
             </div>
         </div>
-    </div>
-	<div class="right-aside">
+    </header>
+	<section class="right-aside">
 		<div class="right-nav">
-		   <div class="main">
-			<ul class="left">
-				<?php wp_nav_menu(array('theme_location' => 'main', 'container' => false, 'container_class' => false)); ?>
-			</ul>
-			<div class="search">
-                <input type="search"  class="input-search" placeholder="搜索">
-                <i class="icon-search"></i>
-            </div>
-			<div class="login"><a href="<?php echo home_url();?>/wp-admin"><i class="icon-user"></i></a></div>
-		 </div>
+		    <nav class="nav-main">
+				<ul class="left">
+					<?php wp_nav_menu(array('theme_location' => 'main', 'container' => false, 'container_class' => false)); ?>
+				</ul>
+				<div class="login"><a href="<?php echo home_url();?>/wp-admin"><i class="icon-user"></i></a></div>
+				<div class="search">
+                	<?php get_search_form(); ?>
+            	</div>
+		 	</nav>
 	    </div>
 		<div class="right-main">
-			<div class="main">
-				<div class="category">
+				<nav class="category">
 					<ul>
 						<?php wp_nav_menu(array('menu'=>'tabNav', 'container' => false, 'container_class' => false)); ?>
 					</ul>
-				</div>
-		    </div>
+		    	</nav>
 			<div class="clear"></div>
 			<div class="list">
-				<div class="main">
 					<?php if(have_posts()) : ?><?php while(have_posts()) : the_post(); ?>
 					<div class="item">
 					<div class="title">
@@ -103,9 +101,8 @@
 
                    <?php endif; ?>
 			  </div>
-		    </div>
 		</div>
-	</div>
+	</section>
 	<script src="<?php bloginfo('template_directory'); ?>/public/js/jquery-1.8.3.min.js"></script>
     <script src="<?php bloginfo('template_directory'); ?>/public/js/MyJs.js"></script>
 </body>
