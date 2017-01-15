fcce6c95a89d7f36e092b9b721fd45bcfc6a43d0
Youthink <hi@hufangyun.com>
2017-01-15 14:34:59 +0800

    初始化目录结构 init

--- VicSugar-wordpress/searchform.php
+++
@@ -1,7 +0,0 @@
-<form class="search" action="<?php echo home_url( "/" ); ?>" method="get">
-
-    <input type="search"  class="input-search" placeholder="搜索" name="s" id="search" value="<?php the_search_query(); ?>" >
-
-    <i class="icon-search"></i>
-
-</form>
