51024a820b34eadb27c931a5615733058a3bb3a9
Youthink <hufy3651@foxmail.com>
2017-04-14 11:37:52 +0800

    js css 使用webpack打包

--- source-src/main.js
+++ source-src/main.js
@@ -1,1 +1,16 @@
 import './main.css';
+
+
+//http 转 https
+var host = "hufangyun.com" || "www.hufangyun.com";
+if ((host == window.location.host) && (window.location.protocol != "https:"))
+    window.location.protocol = "https";
+
+//百度统计
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?ae46047b557574a12bba544bea130043";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,19 @@
 const path = require('path');
 const webpack = require("webpack");
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const minifyHTML = {
+  collapseInlineTagWhitespace: true,
+  collapseWhitespace: true,
+  minifyJS:true
+}
 
 module.exports = {
   entry: './source-src/main.js',
   output: {
     filename: '[name].[chunkhash:6].js',
+    publicPath: "./",
     path: path.resolve(__dirname, 'source')
   },
   module: {
@@ -20,6 +28,20 @@ module.exports = {
         }]
     },
     plugins: [
-       new ExtractTextPlugin('styles.css'),
+       new ExtractTextPlugin('[name].[chunkhash:6].css'),
+       new HtmlWebpackPlugin({
+         inject: false,
+         cache:false,
+         minify: minifyHTML,
+         template: './source-src/script.ejs',
+         filename: '../layout/_partial/script.ejs'
+       }),
+       new HtmlWebpackPlugin({
+         inject: false,
+         cache:false,
+         minify: minifyHTML,
+         template: './source-src/css.ejs',
+         filename: '../layout/_partial/css.ejs'
+       })
     ]
 };
