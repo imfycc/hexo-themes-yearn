277e71f95a8bd99bab800170e5e752dcfdf75a6e
Youthink <hufy3651@foxmail.com>
2017-12-13 14:10:25 +0800

    添加搜索功能

--- source-src/main.js
+++ source-src/main.js
@@ -14,3 +14,83 @@ var _hmt = _hmt || [];
   var s = document.getElementsByTagName("script")[0];
   s.parentNode.insertBefore(hm, s);
 })();
+
+//搜索
+var searchData;
+var search = document.getElementById('search');
+
+if(search) {
+  search.addEventListener('input', function(e) {
+    var key = this.value.trim();
+      if (!key) {
+        return;
+      }
+
+      var regExp = new RegExp(key.replace(/[ ]/g, '|'), 'gmi');
+
+      loadData(function (data) {
+        var result = data.filter(function (post) {
+          return matcher(post, regExp);
+        });
+
+        render(result);
+      });
+
+    e.preventDefault();
+  });
+}
+
+function loadData(success) {
+
+  if (!searchData) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', '/content.json', true);
+
+    xhr.onload = function () {
+      if (this.status >= 200 && this.status < 300) {
+        var res = JSON.parse(this.response);
+        searchData = res instanceof Array ? res : res.posts;
+        success(searchData);
+      } else {
+        console.error(this.statusText);
+      }
+    };
+
+    xhr.onerror = function () {
+      console.error(this.statusText);
+    };
+
+    xhr.send();
+
+  } else {
+    success(searchData);
+  }
+}
+
+function render(data) {
+  var html = '';
+  if (data.length) {
+
+    html = data.map(function (post) {
+        return '<li><a href='+ post.path +'>' + post.title + '</a></li>';
+    });
+
+    if (html) {
+      document.querySelector('.search-result-box').style.display = 'block';
+    }
+    var result = document.getElementById('search-result');
+    result.innerHTML = html.join('');
+  }
+}
+
+function matcher(post, regExp) {
+  return regtest(post.title, regExp);
+}
+
+function regtest(raw, regExp) {
+  regExp.lastIndex = 0;
+  return regExp.test(raw);
+}
+
+
+
