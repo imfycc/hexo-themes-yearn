421d0ff68321f8834244326b3294073772486fbe
Youthink <hufy3651@foxmail.com>
2017-12-14 11:29:28 +0800

    文章页样式优化 && webapck watch 文件修改

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
-
 'use strict';
 
 const path = require('path');
 const webpack = require("webpack");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
@@ -15,53 +15,73 @@ const minifyHTML = {
 module.exports = {
   entry: './source-src/main.js',
   output: {
-    filename: '[name].[chunkhash:6].js',
+    filename: '[name].[hash:6].js',
     publicPath: "./",
     path: path.resolve(__dirname, 'source')
   },
+
+  devtool: 'inline-source-map',
+
   module: {
-        rules: [{
-            test: /\.(scss|sass|css)$/,
-            use: ExtractTextPlugin.extract({
-              fallback: "style-loader",
-              use: [
-                'css-loader',
-                {
-                  loader: 'postcss-loader',
-                  options: {
-                    plugins: () => {
-                      return [
-                       require('autoprefixer')()
-                      ]
-                    }
-                  }
-                },
-                'sass-loader'
-              ]
-            })
-        }]
-    },
-    plugins: [
-       new ExtractTextPlugin('[name].[chunkhash:6].css'),
-       new HtmlWebpackPlugin({
-         inject: false,
-         cache:false,
-         minify: minifyHTML,
-         template: './source-src/script.ejs',
-         filename: '../layout/_partial/script.ejs'
-       }),
-       new HtmlWebpackPlugin({
-         inject: false,
-         cache:false,
-         minify: minifyHTML,
-         template: './source-src/css.ejs',
-         filename: '../layout/_partial/css.ejs'
-       })
-    ],
-    devServer: {
-      contentBase: path.join(__dirname, "source-src"),
-      compress: true,
-      port: 9000,
-      hot: true
-    }
+    rules: [{
+      test: /\.(scss|sass|css)$/,
+      use: ExtractTextPlugin.extract({
+        fallback: "style-loader",
+        use: [
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => {
+                return [
+                  require('autoprefixer')()
+                ]
+              }
+            }
+          },
+          'sass-loader'
+        ]
+      })
+    }]
+  },
+
+  plugins: [
+    new ExtractTextPlugin('[name].[hash:6].css'),
+    new CleanWebpackPlugin(['source']),
+    new HtmlWebpackPlugin({
+      inject: false,
+      cache:false,
+      minify: minifyHTML,
+      template: './source-src/script.ejs',
+      filename: '../layout/_partial/script.ejs'
+     }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      cache:false,
+      minify: minifyHTML,
+      template: './source-src/css.ejs',
+      filename: '../layout/_partial/css.ejs'
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+
+  watch: true
 };
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new CleanPlugin('builds')
+  ])
+}
